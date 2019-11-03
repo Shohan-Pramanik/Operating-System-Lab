84e8e3a7e827b7466bd60047ed579e10251f2201
Shohan-Pramanik <37219879+Shohan-Pramanik@users.noreply.github.com>
2019-11-03 00:19:17 -0700

    Added Shortest Job First Algorithm

---
+++ SJF_algorithm.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,n,tmp;
+    float avg;
+    int a[100],w[100],x=0,t[100]={0};
+    cout<<"\nEnter the number of process : ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cout<<"\nEnter the process p"<<i<<" : ";
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(i=0;i<n;i++)
+    {
+        if(i>0)
+        {
+           w[i]=a[i-1];
+           t[i]=w[i]+t[i-1];
+
+        }
+
+    }
+    for(i=0;i<n;i++)
+    {
+        x=t[i]+x;
+        cout<<"Waiting Time for process p"<<i<<" : "<<t[i]<<endl;
+    }
+    avg=(float)x/n;
+    cout<<"Average Waiting Time : "<<avg<<endl;
+
+    return 0;
+}
+
